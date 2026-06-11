4a3e730c723950520615128c1641712f8af46e9d
Manu Cristini <manu.cristini@epfl.ch>
2026-06-11 08:46:40 +0200

    complete reducer with more passes and more efficient reduction logic

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from reducer_loop import reduce_query
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--query", required=True)
+    parser.add_argument("--reduced", required=True)
     parser.add_argument("--test", required=True)
 
     args = parser.parse_args()
 
-    reduce_query(args.query, args.test)
+    reduce_query(args.query, args.reduced, args.test)
 
 if __name__ == "__main__":
     main()
